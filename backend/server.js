4bdabac78c09660503b11e0e415e8253f8469d32
captainsara <sara.iqbal@hotmail.se>
2022-12-25 11:01:23 +0100

    user authentication, functionality in sign up page and login page

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const { chats } = require('./data/data')
+/* const { chats } = require('./data/data') */
 const connectDB = require('./config/db')
+const userRoutes = require('./routes/userRoutes')
+const {notFound, errorHandler} = require('./middleware/errorMiddelware')
 
 
 
@@ -9,11 +11,20 @@ const app = express()
 dotenv.config()
 connectDB()
 
+//Accept the JSON data fom fronend
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send("API is running")
 })
 
-app.get('/api/chat', (req, res) =>  {
+app.use('/api/user', userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
+
+/* app.get('/api/chat', (req, res) =>  {
   res.send(chats)
 })
 
@@ -21,7 +32,7 @@ app.get('/api/chat/:id', (req, res) => {
   //console.log(req.params.id)
   const singleChat = chats.find((c) => c._id === req.params.id)
   res.send(singleChat)
-})
+}) */
 
 const PORT = process.env.PORT || 5000
 
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,26 +1,79 @@
 import React, { useState } from 'react'
 import { FormControl, FormLabel, VStack, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
+import axios from "axios";
+import { useToast } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
 
 export default function Login() {
   
   const [show, setShow] = useState(false)
+  const handleClick = () => setShow(!show);
+  const toast = useToast();
   const [email, setEmail] = useState()
   const [password, setPassword] = useState()
+  const [loading, setLoading] = useState(false);
 
+  const history = useHistory();
 
-  const handleClick = () => setShow(!show)
+  const submitHandler = async () => {
+    setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
 
- 
+    // console.log(email, password);
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
 
-  const submitHandler = () => {}
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+
+      // console.log(JSON.stringify(data));
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
 
   return (
-    <VStack spacing='5px' color='black'>
-     
-      
+    <VStack spacing='10px'>
       <FormControl id='email' isRequired>
         <FormLabel>E-mail</FormLabel>
-          <Input
+        <Input
+          value={ email }
+          type="email"
             placeholder='Enter your e-mail'
             onChange={(e) => setEmail(e.target.value)}
           />
@@ -30,11 +83,12 @@ export default function Login() {
         <FormLabel>Password</FormLabel>
         <InputGroup size={'md'}>
           <Input
+            value={password}
             type={ show?'text':'password' }
-            placeholder='Enter your Name'
+            placeholder='Enter your password'
             onChange={(e) => setPassword(e.target.value)}
           />
-          <InputRightElement>
+          <InputRightElement width={'4.5rem'}>
             <Button h={'1.75rem'} size='sm' onClick={handleClick}>
               {show ? 'Hide' : 'Show'}
             </Button>
@@ -42,13 +96,12 @@ export default function Login() {
         </InputGroup>
       </FormControl>
 
- 
-
       <Button
         colorScheme='blue'
         width='100%'
         style={{marginTop: 15}}
-        onClick={submitHandler}
+        onClick={ submitHandler }
+        isLoading={loading}
       >
         Login
       </Button>
